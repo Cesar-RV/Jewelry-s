8b851a859ac5721ed153cbfdc23108ba1bdd7d57
ganadoresvio12 <cesarvital664@gmail.com>
2024-04-11 21:01:47 -0600

    Primer commit jewerly

---
+++ src/main/java/com/jewelrygroup/jewerlygroup/JewerlygroupApplication.java
@@ -0,0 +1,13 @@
+package com.jewelrygroup.jewerlygroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewerlygroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewerlygroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jewelrygroup/jewerlygroup/JewerlygroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jewelrygroup.jewerlygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewerlygroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
